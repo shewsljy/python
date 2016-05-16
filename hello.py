c96e95369da337fd1054f71ddd3aed4f0cb38df3
jiayu <1214942360@qq.com>
2016-05-16 23:51:12 +0800

    20160516 at home

--- hello.py
+++ hello.py
@@ -352,3 +352,20 @@ print(isinstance(12345, Iterable))
 
 for i, value in enumerate(['A', 'B', 'C']):
 	print(i, value)
+
+# list comprehensions
+L = [x * x for x in range(1, 11)]
+print(L)
+print([x for x in range(1, 11) if x % 2 == 0])
+print([m + n for m in 'ABC' for n in 'XYZ'])
+import os
+print([d for d in os.listdir('.')])
+
+d = {'a' : 'A', 'b' : 'B', 'c' : 'C'}
+print([k + '=' + v for k, v in d.items()])
+
+L = ['Hello', 'World', 'IBM', 'Apple']
+print([s.lower() for s in L])
+
+L = ['Hello', 'World', 'IBM', 'None', 18, 'Apple']
+print([s.lower() for s in L if isinstance(s, str)])
