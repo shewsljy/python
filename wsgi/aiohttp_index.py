5e5d35c001b165a427357bba0a7466df902575f6
jiayu <1214942360@qq.com>
2016-05-25 18:26:38 +0800

    add orm.py

--- wsgi/aiohttp_index.py
+++ wsgi/aiohttp_index.py
@@ -21,6 +21,7 @@ async def init(loop):
 	app.router.add_route('GET', '/hello/{name}', hello)
 	srv = await loop.create_server(app.make_handler(), '127.0.0.1', 8000)
 	print('Server started at http://127.0.0.1:8000...')
+	return srv
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
