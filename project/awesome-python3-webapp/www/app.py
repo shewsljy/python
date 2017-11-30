739758af82b202340a879485353b06d691b83b41
lijy13 <lijy13@asiainfo.com>
2017-11-30 22:47:52 +0800

    pylint格式化

--- project/awesome-python3-webapp/www/app.py
+++ project/awesome-python3-webapp/www/app.py
@@ -1,27 +1,27 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'jiayu'
+''' async web application. '''
 
-'''
-async web application.
-'''
-
-import logging; logging.basicConfig(level=logging.INFO)
-import asyncio, os, json, time
-from datetime import datetime
+import logging
+import asyncio
 from aiohttp import web
+logging.basicConfig(level=logging.INFO)
+
+__author__ = 'lijiayu'
 
 def index(request):
-	return web.Response(body=b'<h1>Awesome</h1>')
+    ''' 对首页的/进行响应，简单地返回一个Awesome字符串 '''
+    return web.Response(body=b'<h1>Awesome</h1>')
 
 @asyncio.coroutine
 def init(loop):
-	app = web.Application(loop=loop)
-	app.router.add_route('GET', '/', index)
-	srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
-	logging.info('server started at http://127.0.0.1:9000...')
-	return srv
+    ''' web在9000端口监听HTTP请求，并且对首页/进行响应 '''
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
