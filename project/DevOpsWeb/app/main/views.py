3c367fbdb24e3e95f76441e11298e583e9a0a4a6
jiayu <1214942360@qq.com>
2017-12-20 18:29:35 +0800

    navbar

--- project/DevOpsWeb/app/main/views.py
+++ project/DevOpsWeb/app/main/views.py
@@ -5,3 +5,7 @@ from . import main
 @main.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('base.html')
+
+@main.route('/test')
+def test():
+    return render_template('test.html')
